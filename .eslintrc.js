aa195c1008db13eba25487955f2944f306b5cb46
varvara <varvarashcherbyna7@gmail.com>
2022-10-17 19:19:15 +0300

    build: pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,29 @@ module.exports = {
         node: true,
     },
     globals: {
-        Promise: 'readonly',
+        // Promise: 'readonly',
+        React: 'readonly',
     },
+    overrides: [
+        {
+            files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+            rules: {
+                // example of overriding a rule
+                'storybook/hierarchy-separator': 'error',
+            },
+        },
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         sourceType: 'module',
         tsconfigRootDir: __dirname,
         // project: ['./tsconfig.eslint.json'],
     },
+    // rules: {
+    //     '@typescript-eslint/no-var-requires': 0,
+    // },
     rules: {
-        '@typescript-eslint/no-var-requires': 0,
+        'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
     },
     plugins: ['@typescript-eslint', 'react', 'react-hooks'],
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
